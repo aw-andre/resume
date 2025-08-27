f7eb73eb3bc85e9a151d9df25bf91a9c2d151841
aw-andre <aw.andregerard@gmail.com>
2025-08-27 18:15:17 -0400

    chore: remove artifacts

--- flake.nix
+++ flake.nix
@@ -6,7 +6,9 @@
   outputs = { nixpkgs, ... }:
     let pkgs = nixpkgs.legacyPackages."x86_64-linux";
     in {
-      devShells."x86_64-linux".default =
-        pkgs.mkShell { packages = with pkgs; [ texliveFull ]; };
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        packages = with pkgs;
+          [ texliveFull ] ++ (with python313Packages; [ python ipython ipdb ]);
+      };
     };
 }
