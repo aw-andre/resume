842db3d39f0a0c615d0ae1c89370565460c9cb27
aw-andre <aw.andregerard@gmail.com>
2025-08-30 19:26:50 -0400

    refactor: change Native messaging name

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,13 +1,13 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log("Message received in background script:", request);
   if (request.text) {
-    const port = chrome.runtime.connectNative('com.google.chrome.example.echo');
-    port.onMessage.addListener(response => {
-      console.log('Received from native script:', response);
+    const port = chrome.runtime.connectNative("com.highlighter.echo");
+    port.onMessage.addListener((response) => {
+      console.log("Received from native script:", response);
     });
     port.onDisconnect.addListener(() => {
       if (chrome.runtime.lastError) {
-        console.error('Disconnect error:', chrome.runtime.lastError.message);
+        console.error("Disconnect error:", chrome.runtime.lastError.message);
       }
     });
     port.postMessage({ text: request.text });
